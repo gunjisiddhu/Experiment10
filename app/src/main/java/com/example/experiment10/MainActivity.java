723938d82dceaf64f94c24ca282d5e4aa860d4a3
Siddhu <gunjisiddhu@gmail.com>
2022-05-18 12:25:57 +0530

    Drawer Working

--- app/src/main/java/com/example/experiment10/MainActivity.java
+++ app/src/main/java/com/example/experiment10/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.experiment10;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("ALL EXPERIMENTS");
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawerLayout,toolbar,R.string.navigation_bar_open,R.string.navigation_bar_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
